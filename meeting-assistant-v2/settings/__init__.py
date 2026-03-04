a552be1eaae52eafd37eafb524d2883b7ba21c55
wuguipeng <wuguipeng@qq.com>
2026-03-04 21:08:40 +0800

    【删除】冗余的代码

--- meeting-assistant-v2/settings/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/ python
-# -*- encoding: utf-8 -*-
-"""
-Author: Hman.
-Time: 2024/12/19.
-"""
--- meeting-assistant-v2/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/ python
-# -*- encoding: utf-8 -*-
-"""
-Author: Hman.
-Time: 2024/08/14.
-"""
